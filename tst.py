d05a4e02f783d88bcccbdac15d6514fc4844ebb7
Tomas Chiari <tomaschiari01@gmail.com>
2022-04-27 15:25:09 -0300

    cambio develop

--- tst.py
+++ tst.py
@@ -1,4 +1,5 @@
-import math 
+import math
+from traceback import print_tb 
 
 def prueba():
 	pass
@@ -6,3 +7,4 @@ def prueba():
 print("hola mundo")
 
 print("este es un cambio que no estara en el master")
+print("este es un cambio q hago en el develop")
